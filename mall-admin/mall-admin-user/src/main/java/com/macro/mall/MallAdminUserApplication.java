375da7f91706dace860521a475248415ec117505
evenwjc <1161364818@qq.com>
2020-05-31 17:33:17 +0800

    mall电商项目最初版本

---
+++ mall-admin/mall-admin-user/src/main/java/com/macro/mall/MallAdminUserApplication.java
@@ -0,0 +1,18 @@
+package com.macro.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 应用启动入口
+ * Created by macro on 2018/4/26.
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MallAdminUserApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(MallAdminUserApplication.class, args);
+    }
+}
